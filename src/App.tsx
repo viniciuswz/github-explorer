57c42f0e1c46f8707aa76cc751f22b68a650ec5f
vinicius Araujo <vinicius.araujo@agenciaeconverse.com.br>
2020-12-07 20:12:42 -0300

    feature(styled components)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-
 import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyle from './styles/global';
 
 import Routes from './routes/index';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+      <GlobalStyle />
+    </>
   );
 }
 
